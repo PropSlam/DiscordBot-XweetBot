c6a8cebaa6e0d464d179c7fef87626ec93e9e06c
Charlie <chaoticcow@gmail.com>
2023-12-06 20:11:48 -0500

    Now ignores fxtwitter links. Minor code cleanup.

--- XweetBot/Program.cs
+++ XweetBot/Program.cs
@@ -8,77 +8,74 @@ public partial class Program {
     public static Task Main( string[] args ) {
         return new Program().MainAsync();
     }
-    
+
     DiscordSocketClient? _client;
 
-    const string URL_REGEX_STRING = @"https?://[^\s]*(twitter|x)[^\s]+/status/[^\s]+";
-
-    readonly Regex _urlRegex = new(@"https?://[^\s]*(twitter|x)[^\s]+/status/[^\s]+");
-    readonly string[] _targetUrls = new string[]{ "x.com", "twitter.com" };
+    readonly Regex _urlRegex = new(@"https?://(?!fxtwitter)[^\s]*(twitter|x)[^\s]+/status/[^\s]+");
+    readonly string[] _targetUrls = { "x.com", "twitter.com" };
 
     public async Task MainAsync() {
         var root = Directory.GetCurrentDirectory();
-        var dotenv = Path.Combine(root, ".env");
-        DotEnv.Load(dotenv);
-        
+        var dotenv = Path.Combine( root, ".env" );
+        DotEnv.Load( dotenv );
+
         DiscordSocketConfig config = new() {
             UseInteractionSnowflakeDate = false,
             GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent,
         };
         _client = new DiscordSocketClient( config );
-        _client.Log += Log;
+        _client.Log += LogAsync;
         _client.MessageReceived += MessageReceived;
-        
-        await _client.LoginAsync( TokenType.Bot, Environment.GetEnvironmentVariable("XWEETBOT_TOKEN")  );
+
+        await _client.LoginAsync( TokenType.Bot, Environment.GetEnvironmentVariable( "XWEETBOT_TOKEN" ) );
         await _client.StartAsync();
-        await _client.SetGameAsync( "For X/Twitter.com", null, ActivityType.Watching );
-        
-        var log = new LogMessage( LogSeverity.Debug, "MainAsync", "MainAsync Started." );
-        await Log( log );
-        
+        await _client.SetGameAsync( "For X/Twitter Links", null, ActivityType.Watching );
+
+        await LogAsync( new LogMessage( LogSeverity.Info, "MainAsync", "[XweetBot]: Main has started." ) );
+
         // Block this task until the program is closed.
         await Task.Delay( -1 );
     }
-    
+
     async Task MessageReceived( SocketMessage message ) {
         if ( _client != null && message.Author.Id == _client.CurrentUser.Id ) return;
         if ( message is SocketUserMessage userMessage ) {
-            var urls = ExtractURL( userMessage.Content );
+            var urls = ExtractUrl( userMessage.Content );
             if ( urls.Length == 0 ) return;
-            var newMessage = "";
+            var convertedMessage = "";
             foreach ( var url in urls ) {
                 if ( string.IsNullOrEmpty( url ) ) continue;
-                if ( TryConvertURL( url, out var converted ) ) {
-                    newMessage += converted + " ";
+                if ( TryConvertUrl( url, out var converted ) ) {
+                    convertedMessage += converted + " ";
                 }
             }
-            if ( string.IsNullOrEmpty( newMessage ) ) return;
-            await userMessage.ReplyAsync( newMessage );
+
+            if ( string.IsNullOrEmpty( convertedMessage ) ) return;
+            await userMessage.ReplyAsync( convertedMessage );
         }
     }
-    
-    string[] ExtractURL( string message ) {
-        var extracted = _urlRegex.Matches(message)
-            .Cast<Match>()
-            .Select(m => m.Value) 
+
+    string[] ExtractUrl( string message ) {
+        var extracted = _urlRegex.Matches( message )
+            .Select( m => m.Value )
             .ToArray();
         return extracted.Length > 0 ? extracted : Array.Empty<string>();
     }
 
-    bool TryConvertURL( string url, out string converted ) {
+    bool TryConvertUrl( string url, out string converted ) {
         foreach ( var turl in _targetUrls ) {
             if ( !url.Contains( turl ) ) continue;
             converted = url.Replace( turl, "fxtwitter.com" );
-            Console.WriteLine($"[TryConvertURL]: Converted URL: {converted}");
+            LogAsync( new LogMessage( LogSeverity.Info, "TryConvertUrl", "[XweetBot]: Converted Url." ) );
             return true;
         }
+
         converted = "";
         return false;
     }
-    
-    Task Log( LogMessage msg ) {
+
+    static Task LogAsync( LogMessage msg ) {
         Console.WriteLine( msg.ToString() );
         return Task.CompletedTask;
     }
-    
 }
